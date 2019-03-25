0183cdae1f670eecb7e003b8f52378cb5a26752a
didier <didier.colaes@student.ap.be>
2019-03-25 15:30:15 +0100

    fix
    geprobeerd django te upgraden en hier veel tijd aan verloren

--- examenvraag2/brexitApp/urls.py
+++ examenvraag2/brexitApp/urls.py
@@ -1,9 +1,8 @@
 from . import views
 from django.conf.urls import url
-from django.urls import path, include
 
 app_name = 'brexitApp'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    url('', views.index, name='index')
 ]

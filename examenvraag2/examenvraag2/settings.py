80078a92da0ae34942d227dc58743b1a931894e1
didier <didier.colaes@student.ap.be>
2019-03-25 15:42:13 +0100

    installed apps error fixed

--- examenvraag2/examenvraag2/settings.py
+++ examenvraag2/examenvraag2/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'brexitApp.apps.BrexitappConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

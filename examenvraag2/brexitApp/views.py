898ac882f08b95c6d9959e1f45f101232abe8990
didier <didier.colaes@student.ap.be>
2019-03-25 15:51:57 +0100

    final fix

--- examenvraag2/brexitApp/views.py
+++ examenvraag2/brexitApp/views.py
@@ -10,4 +10,13 @@ def index (request):
     time = datetime.datetime.now() 
     ukTime = datetime.datetime(2019, 3,29, 23)
     delta = ukTime - time
-    return render(request, 'index.html', {'time': delta})
+    year = delta.year
+    month = delta.month
+    day = delta.day
+    hour = delta.hour
+    minutes = delta.minute
+    seconds = delta.second
+
+
+
+    return render(request, 'index.html', {'years': year,'months': month, 'days': day, 'hours': hour, 'minutes': minutes, 'seconds': seconds})
--- examenvraag2/examenvraag2/settings.py
+++ examenvraag2/examenvraag2/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'brexitApp.apps.BrexitappConfig'
+    'brexitApp.apps.BrexitappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
